bd7c8bbd55b638dc14cab27a24f7d6484cca03ab
Damian Rzeszot <damian.rzeszot@gmail.com>
2022-05-27 23:56:55 +0200

    Update

--- .github/scripts/spec/check-for-updates.sh
+++ .github/scripts/spec/check-for-updates.sh
@@ -1,27 +1,24 @@
 #!/bin/sh
 
 spec_file="spec.json"
+echo "::set-output name=spec_file::$spec_file"
 
 mkdir tmp
 cd tmp
 
 echo "::group::Download"
 wget https://developer.apple.com/sample-code/app-store-connect/app-store-connect-openapi-specification.zip
+spec_original_file=`ls *.json`
+echo "::set-output name=spec_original_file::$spec_original_file"
 echo "::endgroup::"
 
 echo "::group::Unzip"
-unzip app-store-connect-openapi-specification.zip
+unzip "$spec_original_file"
+spec_version=`cat "$spec_original_file" | jq -r .info.version`
+echo ::set-output name=spec_version::$spec_version
 echo "::endgroup::"
-
-spec_original_file=`ls *.json`
 
 mv "$spec_original_file" "../$spec_file"
 
 cd ..
 rm -rf tmp
-
-spec_version=`cat "$spec_original_file" | jq -r .info.version`
-
-echo ::set-output name=spec_file::$spec_file
-echo ::set-output name=spec_original_file::$spec_original_file
-echo ::set-output name=spec_version::$spec_version
